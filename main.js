097c8c18d8cea15fbf2d556aa61a933209028243
Mario Yanez <marioy705@gmail.com>
2022-08-07 07:57:06 +0800

    Comento mi js para saber qué hice

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //Primero seleccionamos los elementos con los que vamos a trabajar, en este caso donde le daremos click
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector ('.desktop_menu');
-
+//Luego creo mi event listener con el evento a escuchar y la función a realizar
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
+//y creo mi función a realizar.
 function toggleDesktopMenu() {
     console.log('click')
     desktopMenu.classList.toggle('inactive');
