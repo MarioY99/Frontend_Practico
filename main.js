173ff2cbaca40755674cdb9592ecd50eb6170008
Mario Yanez <marioy705@gmail.com>
2022-08-09 03:31:27 +0800

    Modifiqué que el aside detalle de producto no se cierre cuando se le da a otra imagen, sino que se cierre con el botón de cierre que le colocamos

--- main.js
+++ main.js
@@ -8,10 +8,14 @@ const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.my-order');
 const cardsContainer = document.querySelector('.cards_container');
 const asideDetail = document.querySelector('.product-detail');
-const closeAsideDetail = document.querySelector('.product-detail_close');
+const productDetailCloseIcon = document.querySelector('.product-detail_close');
 
 //Luego creo mi event listener con el evento a escuchar y la función a realizar
 menuEmail.addEventListener('click', toggleDesktopMenu);
+mobileMenu.classList.remove('activo');
+burguerMenu.addEventListener('click', toggleMobileMenu);
+menuShoppingCart.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeAsideDetail);
 //y creo mi función a realizar.
 function toggleDesktopMenu() {
     const isAsideClosed = aside.classList.contains('inactive');
@@ -24,11 +28,6 @@ function toggleDesktopMenu() {
     };
     desktopMenu.classList.toggle('inactive');
 };
-
-mobileMenu.classList.remove('activo');
-
-burguerMenu.addEventListener('click', toggleMobileMenu);
-
 function toggleMobileMenu (){
     const isAsideClosed = aside.classList.contains('inactive');
     const isAsideDetailClosed = asideDetail.classList.contains('inactive');
@@ -40,8 +39,6 @@ function toggleMobileMenu (){
     };
     mobileMenu.classList.toggle('activo')
 }
-
-menuShoppingCart.addEventListener('click', toggleCarritoAside);
 function toggleCarritoAside() {
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
     const isMenuMobileClosed = mobileMenu.classList.contains('inactive');
@@ -57,9 +54,10 @@ function toggleCarritoAside() {
     };
     aside.classList.toggle('inactive')
 };
-
-closeAsideDetail.addEventListener('click', toggleAsideDetail);
-function toggleAsideDetail() {
+function closeAsideDetail() {
+    asideDetail.classList.add('inactive');
+}
+function openAsideDetail() {
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
     const isMenuMobileClosed = mobileMenu.classList.contains('inactive');
     const isAsideClosed = aside.classList.contains('inactive');
@@ -72,10 +70,9 @@ function toggleAsideDetail() {
     if(!isAsideClosed) {
         aside.classList.add('inactive');
     }
-    asideDetail.classList.toggle('inactive');
+    asideDetail.classList.remove('inactive');
 };
 //Generalmente las grandes compañías como mercado libre o amazon tienen una forma de trabajar en la que nuestro código recibe un array del backend que está conectado a la base de datos por medio de una API Rest la cual le manda cada artículo y lo va agregando al código del artículo y creando así cada uno de los articulos a mostrar en nuestra página de ventas.
-
 const productList = [];
 productList.push({
     name: 'Bike',
@@ -154,7 +151,6 @@ productList.push({
 });
 
 //Cuando estamos trabajando en un entorno profesional ya, que usamos nuestro código javascript para crear elementos de html y demás, lo mejor es usar ese bloque de código dentro de una función al momento de crearlo porque de esa manera se puede usar código nuevamente y se puede llevar un orden, por ejemplo, en este caso tenemos una lista de objetos, pero si queremos tener una lista de objetos completamente diferente, podríamos hacer uso de esa función y ya mandandole como argumento el array de esa lista de objetos nuevos.
-
 function renderProducts(arr) {
     for (product of arr) {
         const productCard = document.createElement('div');
@@ -162,7 +158,7 @@ function renderProducts(arr) {
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
-        productImg.addEventListener('click', toggleAsideDetail);
+        productImg.addEventListener('click', openAsideDetail);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product_info');
@@ -191,7 +187,5 @@ function renderProducts(arr) {
         cardsContainer.appendChild(productCard)
     }
 };
-
 //De esta forma, llamamos a nuestra función, ya con el array que le queramos mandar y funcionaría perfecto, esto nos ayudaría si por ejemplo hacemos click en la parte de "ROPAS" o la parte de la tienda que muestra solo ELECTRONICS. y hacer uso de esta misma función pero ya con los elementos de electronics que estarían en un array específico de electronics en el que usaríamos nuestro código JavaScript en este caso nuestra función, solo mandandole el argumento del array de electronics para que se muestre en pantalla de manera ordenada cada elemento de electronics para nuestros usuarios.
-
 renderProducts(productList);
